575e5df6800c9a6c31dd3f427b813f503c2ce823
Gudata <i.bardarov@gmail.com>
2012-06-14 17:03:40 +0300

    Bulgarian translation

---
+++ vendor/assets/javascripts/ui/i18n/jquery.ui.datepicker-bg.js
@@ -0,0 +1,23 @@
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
+jQuery(function($){
+	$.datepicker.regional['ru'] = {
+		closeText: 'Затвори',
+		prevText: '&#x3c;Пред',
+		nextText: 'След&#x3e;',
+		currentText: 'Днес',
+		monthNames: ['Януари','Февруари','Март','Април','Май','Юни',
+		'Юли','Август','Септември','Октомври','Ноември','Декември'],
+		monthNamesShort: ['Яну.','Фев','Мар','Апр','Май','Юни',
+		'Юли','Авг','Сеп','Окт','Ное','Дек'],
+		dayNames: ['Неделя','понеделник','вторник','сряда','четвъртък','петък','събота'],
+		dayNamesShort: ['нед','пон','втр','сяд','чтв','пет','съб'],
+		dayNamesMin: ['Не','Пн','Вт','Ср','Чт','Пт','Сб'],
+		weekHeader: 'Се',
+		dateFormat: 'dd-mm-yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: 'г.'};
+	$.datepicker.setDefaults($.datepicker.regional['bg']);
+});
